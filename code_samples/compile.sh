40077cd2a0b75a88357407bef27bca4939f88739
csart03 <arthraj9@gmail.com>
2022-06-07 19:15:46 +0530

    Changes fixed

--- code_samples/compile.sh
+++ code_samples/compile.sh
@@ -1,10 +1,10 @@
-g++ 001_people_tracking.cpp /usr/src/tensorrt/bin/common/logger.o  `pkg-config --libs objdet` `pkg-config --libs --cflags opencv python3 libusb-1.0`   -O3  -o 001_people_tracking.out -I../include/ -lv4l2 -lpthread -lcudart -L/usr/local/cuda/lib64 -lnvinfer -I../../monolith/inc -w -std=c++11
+g++ 001_people_tracking.cpp `pkg-config --libs --cflags opencv python3 libusb-1.0 objdet` -O3  -o 001_people_tracking.out 
 
-g++ 002_people_following.cpp /usr/src/tensorrt/bin/common/logger.o  `pkg-config --libs objdet` `pkg-config --libs --cflags opencv python3 libusb-1.0`   -O3  -o 002_people_following.out -I../include/ -lv4l2 -lpthread -lcudart -L/usr/local/cuda/lib64 -lnvinfer -I../../monolith/inc -w -std=c++11
+g++ 002_people_following.cpp `pkg-config --libs --cflags opencv python3 libusb-1.0 objdet` -O3  -o 002_people_following.out 
 
-g++ 003_object_tracking.cpp /usr/src/tensorrt/bin/common/logger.o  `pkg-config --libs objdet` `pkg-config --libs --cflags opencv python3 libusb-1.0`   -O3  -o 003_object_tracking.out -I../include/ -lv4l2 -lpthread -lcudart -L/usr/local/cuda/lib64 -lnvinfer -I../../monolith/inc -w -std=c++11
+g++ 003_object_tracking.cpp `pkg-config --libs --cflags opencv python3 libusb-1.0 objdet` -O3  -o 003_object_tracking.out 
 
-g++ 004_object_following.cpp /usr/src/tensorrt/bin/common/logger.o  `pkg-config --libs objdet` `pkg-config --libs --cflags opencv python3 libusb-1.0`   -O3  -o 004_object_following.out -I../include/ -lv4l2 -lpthread -lcudart -L/usr/local/cuda/lib64 -lnvinfer -I../../monolith/inc -w -std=c++11
+g++ 004_object_following.cpp `pkg-config --libs --cflags opencv python3 libusb-1.0 objdet` -O3  -o 004_object_following.out 
 
-g++ 005_object_detection.cpp /usr/src/tensorrt/bin/common/logger.o  `pkg-config --libs objdet` `pkg-config --libs --cflags opencv python3 libusb-1.0`   -O3  -o 005_object_detection.out -I../include/ -lv4l2 -lpthread -lcudart -L/usr/local/cuda/lib64 -lnvinfer -I../../monolith/inc -w -std=c++11
+g++ 005_object_detection.cpp `pkg-config --libs --cflags opencv python3 libusb-1.0 objdet` -O3  -o 005_object_detection.out 
 
