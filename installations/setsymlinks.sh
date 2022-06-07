aa1b2b113d1e1f4ca9d28f19ace177287583655c
csart03 <arthraj9@gmail.com>
2022-06-07 13:54:00 +0530

    setsymlinks.sh fixed

--- installations/setsymlinks.sh
+++ installations/setsymlinks.sh
@@ -1,8 +1,6 @@
-cd /usr/local/lib
-sudo ln -s $OLDPWD/../lib/libPAL_3dObjDet.so libPAL_3dObjDet.so
-sudo ln -s $OLDPWD/../lib/libPAL_DATA_3dObjDet.so libPAL_DATA_3dObjDet.so
-sudo ln -s $OLDPWD/../lib/libPAL_DE_3dObjDet.so libPAL_DE_3dObjDet.so
-sudo ln -s $OLDPWD/../lib/libPAL_DEPTH_128_3dObjDet.so libPAL_DEPTH_128_3dObjDet.so
-sudo ln -s $OLDPWD/../lib/libPAL_DEPTH_HQ_3dObjDet.so libPAL_DEPTH_HQ_3dObjDet.so
-sudo ln -s $OLDPWD/../lib/libPAL_Track_3dObjDet.so libPAL_Track_3dObjDet.so
-cd -
+sudo cp ../lib/libPAL_3dObjDet.so /usr/local/lib
+sudo cp ../lib/libPAL_DATA_3dObjDet.so /usr/local/lib
+sudo cp ../lib/libPAL_DE_3dObjDet.so /usr/local/lib
+sudo cp ../lib/libPAL_DEPTH_128_3dObjDet.so /usr/local/lib
+sudo cp ../lib/libPAL_DEPTH_HQ_3dObjDet.so /usr/local/lib
+sudo cp ../lib/libPAL_Track_3dObjDet.so /usr/local/lib
