76e7ce07b2ce780bc492a6ed40f8811cf0927a6b
romil611 <romil.aggarwal@dreamvu.com>
2022-06-14 21:37:48 +0530

    v4.1.2 ER-mode and 0 fov fix, and NANO support

--- installations/bin_files/build_engines.sh
+++ installations/bin_files/build_engines.sh
@@ -24,6 +24,32 @@ then
     fi
 fi
 
+JETSON_DTS="$(tr -d '\0' < /proc/device-tree/nvidia,dtsfilename | sed 's/.*\///')"
+
+case $JETSON_DTS in
+    *2180*) JETSON_TYPE="TX1" ;;
+    *3310*) JETSON_TYPE="TX2" ;;
+    *3489-0080*) JETSON_TYPE="TX2 4GB" ;;
+    *3489*) JETSON_TYPE="TX2i" ;;
+    *2888-0006*) JETSON_TYPE="AGX Xavier [8GB]" ;;
+    *2888-0001*) JETSON_TYPE="AGX Xavier [16GB]" ;;
+    *2888-0004*) JETSON_TYPE="AGX Xavier [32GB]" ;;
+    *2888-0005*) JETSON_TYPE="AGX Xavier [64GB]" ;;
+    *2888*) JETSON_TYPE="AGX Xavier [32GB]" ;;
+    *3448-0002*) JETSON_TYPE="Nano" ;;
+    *3448*) JETSON_TYPE="Nano (Developer Kit Version)" ;;
+    *3668-0001*) JETSON_TYPE="Xavier NX" ;;
+    *3668-0003*) JETSON_TYPE="Xavier NX [16GB]" ;;
+    *3668*) JETSON_TYPE="Xavier NX (Developer Kit Version)" ;;
+    *3701*) JETSON_TYPE="Jetson AGX Orin (Developer Kit Version)" ;;
+    *) JETSON_TYPE="" ;;
+esac
+
+UseDLA=1
+case $JETSON_TYPE in
+    *Nano*) UseDLA=0;;
+esac
+
 a=1
 success=""
 failed=""
@@ -34,32 +60,46 @@ do
     if [ "$a" -eq 1 ]
     then
         b=engine_depth128.trt
-        trtexec --onnx=./generate/$a.bin --int8 --fp16 --calib=./generate/$a.cache --verbose --useDLACore=0 --allowGPUFallback  --saveEngine=/usr/local/bin/data/$b --workspace=$1       
+        if ["$UseDLA" -eq 1 ] ; then
+            trtexec --onnx=./generate/$a.bin --int8 --fp16 --calib=./generate/$a.cache --verbose --useDLACore=0 --allowGPUFallback  --saveEngine=/usr/local/bin/data/$b --workspace=$1       
+        else
+            trtexec --onnx=./generate/$a.bin --int8 --fp16 --calib=./generate/$a.cache --verbose --saveEngine=/usr/local/bin/data/$b --workspace=$1   
+        fi    
     fi
     if [ "$a" -eq 2 ]
     then
         b=engine_floor.trt 
-        trtexec --onnx=./generate/$a.bin --int8 --fp16 --calib=./generate/$a.cache --verbose --useDLACore=1 --allowGPUFallback  --saveEngine=/usr/local/bin/data/$b --workspace=$1
+        if ["$UseDLA" -eq 1 ] ; then
+            trtexec --onnx=./generate/$a.bin --int8 --fp16 --calib=./generate/$a.cache --verbose --useDLACore=1 --allowGPUFallback  --saveEngine=/usr/local/bin/data/$b --workspace=$1
+        else
+            trtexec --onnx=./generate/$a.bin --int8 --fp16 --calib=./generate/$a.cache --verbose --saveEngine=/usr/local/bin/data/$b --workspace=$1
+        fi
+
     fi
-    if [ "$a" -eq 3 ]
-    then
+    if [ "$a" -eq 3 ] ; then
         b=engine_track.trt
-        trtexec --onnx=./generate/$a.bin --fp16 --batch=1 --verbose --saveEngine=/usr/local/bin/data/$b --workspace=$1
+        trtexec --onnx=./generate/$a.bin --fp16 --verbose --saveEngine=/usr/local/bin/data/$b --workspace=$1
     fi
     if [ "$a" -eq 4 ]
     then
         b=engine_depth384.trt
-        trtexec --onnx=./generate/$a.bin --int8 --fp16 --calib=./generate/$a.cache --verbose --useDLACore=0 --allowGPUFallback  --saveEngine=/usr/local/bin/data/$b --workspace=$1       
+        if ["$UseDLA" -eq 1 ] ; then
+            trtexec --onnx=./generate/$a.bin --int8 --fp16 --calib=./generate/$a.cache --verbose --useDLACore=0 --allowGPUFallback  --saveEngine=/usr/local/bin/data/$b --workspace=$1       
+        else
+            trtexec --onnx=./generate/$a.bin --int8 --fp16 --calib=./generate/$a.cache --verbose --saveEngine=/usr/local/bin/data/$b --workspace=$1       
+        fi
     fi
     if [ "$a" -eq 5 ]
     then
         b=engine_hqdec.trt
-        trtexec --onnx=./generate/$a.bin --fp16 --batch=1 --verbose --saveEngine=/usr/local/bin/data/$b --workspace=$1
+        if ["$UseDLA" -eq 1 ] ; then
+            trtexec --onnx=./generate/$a.bin --fp16 --verbose --saveEngine=/usr/local/bin/data/$b --workspace=$1
+        fi
     fi
     if [ "$a" -eq 6 ]
     then
         b=dec1.trt
-        trtexec --onnx=./generate/$a.bin --fp16 --batch=1 --verbose --saveEngine=/usr/local/bin/data/$b --workspace=$1
+        trtexec --onnx=./generate/$a.bin --fp16 --verbose --saveEngine=/usr/local/bin/data/$b --workspace=$1
     fi
 
     if [ $? -eq 0 ]; then 
