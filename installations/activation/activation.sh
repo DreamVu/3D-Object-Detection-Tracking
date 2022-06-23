5ad20fcc200ac0dc1c84317b3c461900f9b870f0
Romil Aggarwal <romilaggarwal611@gmail.com>
2022-06-23 16:08:34 +0530

    Update activation.sh
    Corrected er-files being copied to wrong location

--- installations/activation/activation.sh
+++ installations/activation/activation.sh
@@ -10,8 +10,9 @@ if test -f "$File1"; then
     cp Res2.png /data/lut/
     cp Res2.png ../bin_files/root-data/data/lut/
     cp unit.txt /usr/local/bin/data
-    cp -r er-files/ /data/lut/er-files
-    cp -r er-files/ ../bin_files/root-data/data/lut/er-files
+    
+    cp -r er-files/ /data/lut/
+    cp -r er-files/ ../bin_files/root-data/data/lut/
     
     rm -rf er-files
     rm Res2.png data.zip unit.txt
@@ -29,6 +30,3 @@ if $validated; then
     rm -rf data	   
     echo "[PAL:INFO] Camera data files are installed successfully." 
 fi
- 
-
-
