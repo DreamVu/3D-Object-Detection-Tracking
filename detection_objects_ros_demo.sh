55eb4dc1541c4457c2826c499e7ce7fe67e7521e
debashish05 <debashish.roy1998@gmail.com>
2022-08-05 19:04:45 +0530

    removed dreamvu pal tracking folder, since it is part of debian package, updated libtrack lib file, updated ros scripts to run in new deb package setup

--- detection_objects_ros_demo.sh
+++ detection_objects_ros_demo.sh
@@ -1,9 +1,5 @@
-cd installations 
-./ros_cmake.sh
-cp -r ../dreamvu_pal_tracking/ ~/catkin_ws/src/
 cd ~/catkin_ws/
 source ~/catkin_ws/devel/setup.bash
 catkin_make
 source ~/catkin_ws/devel/setup.bash
 roslaunch dreamvu_pal_tracking detection_objects_rviz.launch
-
