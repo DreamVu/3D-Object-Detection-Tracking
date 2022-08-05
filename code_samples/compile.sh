c043fa30a9865f537e5139cf8819a7274d26417b
debashish05 <debashish.roy1998@gmail.com>
2022-08-05 18:54:44 +0530

    updated compile script

--- code_samples/compile.sh
+++ code_samples/compile.sh
@@ -1,10 +1,10 @@
-g++ 001_people_tracking.cpp `pkg-config --libs --cflags opencv python3 libusb-1.0 objdet` -O3  -o 001_people_tracking.out 
+g++ 001_people_tracking.cpp `pkg-config --libs --cflags opencv python3 libusb-1.0 objdet-pal-usb` -O3  -o 001_people_tracking.out 
 
-g++ 002_people_following.cpp `pkg-config --libs --cflags opencv python3 libusb-1.0 objdet` -O3  -o 002_people_following.out 
+g++ 002_people_following.cpp `pkg-config --libs --cflags opencv python3 libusb-1.0 objdet-pal-usb` -O3  -o 002_people_following.out 
 
-g++ 003_object_tracking.cpp `pkg-config --libs --cflags opencv python3 libusb-1.0 objdet` -O3  -o 003_object_tracking.out 
+g++ 003_object_tracking.cpp `pkg-config --libs --cflags opencv python3 libusb-1.0 objdet-pal-usb` -O3  -o 003_object_tracking.out 
 
-g++ 004_object_following.cpp `pkg-config --libs --cflags opencv python3 libusb-1.0 objdet` -O3  -o 004_object_following.out 
+g++ 004_object_following.cpp `pkg-config --libs --cflags opencv python3 libusb-1.0 objdet-pal-usb` -O3  -o 004_object_following.out 
 
-g++ 005_object_detection.cpp `pkg-config --libs --cflags opencv python3 libusb-1.0 objdet` -O3  -o 005_object_detection.out 
+g++ 005_object_detection.cpp `pkg-config --libs --cflags opencv python3 libusb-1.0 objdet-pal-usb` -O3  -o 005_object_detection.out 
 
